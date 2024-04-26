e4013ed1e2a682f1aa0d8ceea60097d1216431e9
Pavel <pavelkvarda@email.cz>
2024-04-26 20:24:43 +0200

    RFCT: Remove save to csv tests and methods

--- src/main/java/cz/paful/weightwise/service/CsvDataService.java
+++ src/main/java/cz/paful/weightwise/service/CsvDataService.java
@@ -3,12 +3,12 @@ package cz.paful.weightwise.service;
 import cz.paful.weightwise.data.dto.MeasurementDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -17,8 +17,6 @@ import java.time.format.SignStyle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.time.temporal.ChronoField.*;
 
@@ -28,28 +26,8 @@ public class CsvDataService {
     private static final Logger LOG = LoggerFactory.getLogger(CsvDataService.class);
 
     private static final String DELIMETER = ";";
-    private static String FILE_NAME;
-
-    private final boolean isTest;
-
-    public CsvDataService(@Value("${app.isTest}") boolean isTest) {
-        this.isTest = isTest;
-        FILE_NAME = isTest ? "measurements_test.csv" : "measurements.csv";
-    }
-
-    public List<MeasurementDTO> read() throws FileNotFoundException {
-        return read(new FileInputStream(getFullPath()));
-    }
 
     public List<MeasurementDTO> read(InputStream inputStream) {
-        String path = getFullPath();
-        LOG.info("Reading data from " + path);
-
-        if (!Files.exists(Paths.get(path))) {
-            LOG.error("File doesnt exits!");
-            return new ArrayList<>();
-        }
-
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
@@ -88,63 +66,8 @@ public class CsvDataService {
         return measurements;
     }
 
-    public void write(List<MeasurementDTO> measurements) throws IOException {
-        List<String[]> dataLines = new ArrayList<>();
-        for (MeasurementDTO measurement : measurements) {
-            String[] data = {
-                    getDateFormatter().format(measurement.getDate()),
-                    getTimeFormatter().format(measurement.getTime()),
-                    String.valueOf(measurement.getWeightKg()),
-                    String.valueOf(measurement.getBmi()),
-                    String.valueOf(measurement.getBodyFatProcentage()),
-                    String.valueOf(measurement.getWaterProcentage()),
-                    String.valueOf(measurement.getMuscleMassProcentage()),
-                    String.valueOf(measurement.getBonesKg()),
-                    measurement.getComment(),
-                    measurement.getMedication()
-            };
-            dataLines.add(data);
-        }
-
-        for (String[] line : dataLines) {
-            LOG.info("WRITING: " + Arrays.toString(line));
-        }
-
-        File csvOutputFile = new File(getFullPath());
-        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            dataLines.stream()
-                    .map(this::convertToCSV)
-                    .forEach(pw::println);
-        }
-    }
-
-    private String convertToCSV(String[] data) {
-        return Stream.of(data)
-                .map(this::escapeSpecialCharacters)
-                .collect(Collectors.joining(DELIMETER));
-    }
-
-    private String escapeSpecialCharacters(String data) {
-        if (data == null) {
-            throw new IllegalArgumentException("Input data cannot be null");
-        }
-        String escapedData = data.replaceAll("\\R", " ");
-        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
-            data = data.replace("\"", "\"\"");
-            escapedData = "\"" + data + "\"";
-        }
-        return escapedData;
-    }
-
     private float parseFloat(String value) {
         return Float.parseFloat(value.replace(",", "."));
-    }
-
-    public String getFullPath() {
-        String path = isTest ? "src/test/resources" : "src/main/resources";
-        File file = new File(path + "/" + FILE_NAME);
-        return file.getAbsolutePath();
-        //return new File(FILE_NAME).getAbsolutePath();
     }
 
     public DateTimeFormatter getDateFormatter() {
--- src/test/java/cz/paful/weightwise/data/CsvDataServiceTests.java
+++ src/test/java/cz/paful/weightwise/data/CsvDataServiceTests.java
@@ -1,91 +1,10 @@
 package cz.paful.weightwise.data;
 
-import cz.paful.weightwise.data.dto.MeasurementDTO;
-import cz.paful.weightwise.service.CsvDataService;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.Assert;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @ActiveProfiles("test")
 @SpringBootTest
 public class CsvDataServiceTests {
-
-    @Test
-    void readWriteDataTest() throws IOException {
-        writeData();
-        Assert.notEmpty(readData(), "NO DATA TO READ!");
-    }
-
-    @Test
-    void readAppDataTest() throws FileNotFoundException {
-        CsvDataService csvDataService = new CsvDataService(true);
-        try (PrintWriter out = new PrintWriter(csvDataService.getFullPath())) {
-            out.println(getDataString());
-        }
-        List<MeasurementDTO> result = readData();
-        Assert.isTrue(result.size() == 6, "Data size not match.");
-    }
-
-    private float getRandom(float min, float max) {
-        return (float) (min + Math.random() * (max - min));
-    }
-
-    private List<MeasurementDTO> readData() throws FileNotFoundException {
-        CsvDataService csvDataService = new CsvDataService(true);
-        return csvDataService.read();
-    }
-
-    private void writeData() throws IOException {
-        List<MeasurementDTO> measurementsToWrite = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            MeasurementDTO measurementDTO = new MeasurementDTO();
-            measurementDTO.setDate(LocalDate.now());
-            measurementDTO.setTime(LocalTime.now());
-            measurementDTO.setWeightKg(getRandom(30,150));
-            measurementDTO.setBmi(getRandom(10,40));
-            measurementDTO.setBodyFatProcentage(getRandom(1,90));
-            measurementDTO.setWaterProcentage(getRandom(30,80));
-            measurementDTO.setMuscleMassProcentage(getRandom(10,90));
-            measurementDTO.setBonesKg(getRandom(2,5));
-            measurementDTO.setComment("");
-            measurementDTO.setMedication("");
-
-            measurementsToWrite.add(measurementDTO);
-        }
-
-        CsvDataService csvDataService = new CsvDataService(true);
-        csvDataService.write(measurementsToWrite);
-    }
-
-    private String getDataString() {
-        return "Podrobnosti o uživateli\n" +
-                "Pohlaví muž\n" +
-                "Jméno Pavel\n" +
-                "Příjmení Kvarda\n" +
-                "Datum narození 09/12/1994\n" +
-                "Výška 173 cm\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "Hmotnost\n" +
-                "Datum, Čas, kg, BMI, Tělesný tuk, Voda, Svaly, Kosti, Komentář, Medikace\n" +
-                "14/03/2024;14:30;75,6;25,2;15,9;61,8;42,6;3,3;;\n" +
-                "15/03/2024;05:15;75,6;25,2;15,4;62,4;42,9;3,3;;\n" +
-                "16/03/2024;07:40;74,4;24,8;14,2;63,4;43,6;3,3;;\n" +
-                "17/03/2024;07:49;74,2;24,7;15,0;62,3;43,1;3,3;;\n" +
-                "18/03/2024;07:24;75,4;25,1;13,4;65,1;44,1;3,4;;\n" +
-                "19/03/2024;08:06;75,0;25,0;15,0;62,8;43,1;3,3;;"  ;
-    }
-
 
 }
